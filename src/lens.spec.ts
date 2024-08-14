c38b634049fd694886d8e868ea1a08790702caf2
dempseyt <dempsey.thompson@journeyone.com.au>
2024-08-14 11:55:23 +0800

    GREEN: lens › over should modify the value using the lens

--- src/lens.spec.ts
+++ src/lens.spec.ts
@@ -1,4 +1,4 @@
-import lens, { set, view } from "./lens";
+import lens, { over, set, view } from "./lens";
 
 type Address = {
   city: string;
--- src/lens.ts
+++ src/lens.ts
@@ -11,6 +11,9 @@ export const view = <S, A>(lens: Lens<S, A>, state: S): A => lens.get(state);
 export const set = <S, A>(lens: Lens<S, A>, valueToSet: A, state: S): S =>
   lens.set(valueToSet, state);
 
+export const over = <S, A>(lens: Lens<S, A>, fn: (value: A) => A, state: S) =>
+  lens.set(fn(lens.get(state)), state);
+
 const lens = <S, A>(
   getter: Getter<S, A>,
   setter: Setter<S, A>
