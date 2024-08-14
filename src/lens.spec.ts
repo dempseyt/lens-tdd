eedc2b918ff3dcb4f3db85b98c4c34470264d8ee
dempseyt <dempsey.thompson@journeyone.com.au>
2024-08-14 11:33:05 +0800

    RED: lens › set should update the value using the lens

--- src/lens.spec.ts
+++ src/lens.spec.ts
@@ -15,19 +15,24 @@ describe("lens", () => {
     name: "Alice",
     address: { city: "Wonderland", zip: "12345" },
   };
+  const nameLens = lens<User, string>(
+    (user) => user.name,
+    (newName, user) => ({ ...user, name: newName })
+  );
+  const cityLens = lens<User, string>(
+    (user) => user.address.city,
+    (newCity, user) => ({
+      ...user,
+      address: { ...user.address, city: newCity },
+    })
+  );
   it("view should retrieve the value using the lens", () => {
-    const nameLens = lens<User, string>(
-      (user) => user.name,
-      (newName, user) => ({ ...user, name: newName })
-    );
-    const cityLens = lens<User, string>(
-      (user) => user.address.city,
-      (newCity, user) => ({
-        ...user,
-        address: { ...user.address, city: newCity },
-      })
-    );
     expect(view(nameLens, user)).toBe("Alice");
     expect(view(cityLens, user)).toBe("Wonderland");
+  });
+  it("set should update the value using the lens", () => {
+    const updatedUser = set(nameLens, "Gerald", user);
+    expect(updatedUser).not.toBe(user);
+    expect(view(nameLens, updatedUser)).toBe("Gerald");
   });
 });
